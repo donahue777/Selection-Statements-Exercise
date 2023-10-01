6a9af5059df029b93bfab3832eb2c4b747cc6a1d
Thomas Donahue <donahue777@yahoo.com>
2023-10-01 09:06:19 -0400

    Excercise 1 complete

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Try to guess the correct number.");
+            Console.WriteLine("Input the upper limit for the range of the number.");
+
+            var userResponse = Console.ReadLine();
+            var upperLimit = int.Parse(userResponse);
+
+            var random = new Random();
+            var number = random.Next(1, upperLimit);
+
+            Console.WriteLine("Input your guess.");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Your number is too high.");
+            }
+
+            else if (guess < number)
+            {
+                Console.WriteLine("Your number is too low.");
+            }
+
+            else     
+            {
+                Console.WriteLine("Congrats!! You've guessed the correct number!");
+            }
         }
     }
 }
